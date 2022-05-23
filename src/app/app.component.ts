1831beb53a2e1ae3267a46db1286c20d8c540efb
Jonathan Ndambuki <joendambuki16@gmail.com>
2022-05-23 12:27:28 +0300

    worked on template Driven Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'td-form';
+  genders=['Male', 'Female', 'Other', 'Rather Not say']
+  @ViewChild('form') form!:NgForm
+  onSubmit(){
+    console.log(this.form);
+
+    console.log({
+
+      name:this.form.value.name,
+      gender:this.form.value.gender,
+      course:this.form.value.course,
+      bio:this.form.value.bio,
+      email:this.form.value.contactInfo.email,
+      phone:this.form.value.contactInfo.phone
+    });
+    
+    this.form.resetForm()
+  }
+  populate(){
+    // this.form.setValue({
+    //   bio: "This is my Bio",
+    //   contactInfo: {
+    //     email: 'john@gmail.com',
+    //      phone: '0700000000'
+    //     },
+    //   course: "c+",
+    //   gender: "Male",
+    //   name: "John Doe"
+    // })
+
+    this.form.form.patchValue({
+      contactInfo: {
+            email: 'john@gmail.com',
+             phone: '0700000000'
+            }
+    })
+  }
 }
